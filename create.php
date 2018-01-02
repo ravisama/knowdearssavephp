857040176e3eaf8277c2c9a28e8b83c5aed5beba
ravisama <pastorravisama@gmail.com>
2018-01-02 18:33:36 +0530

    Create create.php

---
+++ create.php
@@ -0,0 +1,326 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Did Your Friends Know You? Play Quiz</title>
+        <meta name="description" content="Create a easy quiz Game for your friends. Let's See How much your friends know you perfectly."/>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Dhillon" />
+        <meta name="copyright" content="Dhillon" />
+        <meta name="robots" content="follow"/>
+        <meta http-equiv="cache-control" content="no-cache"/>
+<meta itemprop="name" content="Did Your Friends Know You? Play Quiz">
+<meta itemprop="description" content="Create a easy quiz Game for your friends. Let's See How much your friends know you perfectly.">
+<meta itemprop="image" content="http://know-you.pw/share.jpg">
+
+<!-- Twitter Card data -->
+<meta name="twitter:card" content="Hello, Guys Play My Quiz Let's See How much you know about me">
+<meta name="twitter:site" content="http://know-you.pw">
+<meta name="twitter:title" content="Did Your Friends Know You? Play Quiz">
+<meta name="twitter:description" content="Create a easy quiz Game for your friends. Let's See How much your friends know you perfectly.">
+<meta name="twitter:creator" content="Dhillon">
+<!-- Twitter summary card with large image must be at least 280x150px -->
+<meta name="twitter:image:src" content="http://know-you.pw/share.jpg">
+
+<!-- Open Graph data -->
+<meta property="og:title" content="Did Your Friends Know You? Play Quiz" />
+<meta property="og:type" content="article" />
+<meta property="og:url" content="http://know-you.pw/" />
+<meta property="og:image" content="http://know-you.pw/share2.jpg" />
+<meta property="og:image:width" content="600px" />
+<meta property="og:image:height" content="315px" />
+
+<meta property="og:description" content="Create a easy quiz Game for your friends. Let's See How much your friends know you perfectly." />
+<meta property="og:site_name" content="know-you.pw" />
+<style>body{
+    margin:0px;
+    background:#f3f5f5;
+}
+h1{
+font: normal 24px/1 Tahoma, Geneva, sans-serif;
+}
+h3{
+font: normal 16px/1 Tahoma, Geneva, sans-serif;
+}
+input[type="text"].foo {
+    text-align:center;
+  display: inline-block;
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  padding: 10px;
+  border: 1px solid #b7b7b7;
+  font: normal normal 16px/normal "Trebuchet MS", Helvetica, sans-serif;
+    border-radius: 8px;
+  color: rgba(83,83,84,1);
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  background: rgba(252,252,252,1);
+  -webkit-box-shadow: 2px 2px 2px 1px rgba(0,0,0,0.2) ;
+  box-shadow: 2px 2px 2px 1px rgba(0,0,0,0.2) ;
+  text-shadow: 1px 1px 0 rgba(255,255,255,0.66) ;
+  -webkit-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  -moz-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  -o-transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+  transition: all 200ms cubic-bezier(0.42, 0, 0.58, 1);
+}
+.ques{
+    display: block;
+-webkit-box-sizing: content-box;
+-moz-box-sizing: content-box;
+box-sizing: content-box;
+float: none;
+z-index: auto;
+width: auto;
+height: auto;
+position: static;
+cursor: default;
+opacity: 1;
+top: 0;
+margin: 30px 11px 11px;
+padding: 20px;
+overflow: hidden;
+border: none;
+-webkit-border-radius: 0;
+border-radius: 0;
+font: normal 18px/1 Tahoma, Geneva, sans-serif;
+color: rgba(76,74,74,1);
+text-align: center;
+-o-text-overflow: ellipsis;
+text-overflow: ellipsis;
+background: rgb(255,255,255);
+-webkit-box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.3) ;
+box-shadow: 1px 1px 2px 1px rgba(0,0,0,0.3) ;
+text-shadow: none;
+-webkit-transition: none;
+-moz-transition: none;
+-o-transition: none;
+transition: none;
+-webkit-transform: none;
+transform: none;
+-webkit-transform-origin: 50% 50% 0;
+transform-origin: 50% 50% 0;
+}
+.top{display: block;
+-webkit-box-sizing: content-box;
+-moz-box-sizing: content-box;
+box-sizing: content-box;
+z-index: 1000;
+height: 5%;
+cursor: default;
+opacity: 1;
+margin: 0;
+padding: 23px;
+overflow: hidden;
+border: none;
+-webkit-border-radius: 0;
+border-radius: 0;
+font: normal 20px "Trebuchet MS", Helvetica, sans-serif;
+color: rgba(255,255,255,1);
+text-align: center;
+-o-text-overflow: ellipsis;
+text-overflow: ellipsis;
+background: #ec176e;
+-webkit-box-shadow: 1px 1px 1px 0 rgba(0,0,0,0.3) ;
+box-shadow: 1px 1px 1px 0 rgba(0,0,0,0.3) ;
+text-shadow: 1px 1px 1px rgba(0,0,0,0.2) ;
+-webkit-transition: none;
+-moz-transition: none;
+-o-transition: none;
+transition: none;
+-webkit-transform: none;
+transform: none;
+-webkit-transform-origin: 50% 50% 0;
+transform-origin: 50% 50% 0;
+  text-decoration: none;
+  border-bottom: 2.5px solid #fdbe2c;
+}
+form{
+    display: inline;
+}
+.btn {
+    margin:10px;
+-webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  cursor: pointer;
+  padding: 11px 20px  11px 20px;
+  border: 1px solid #ff0085;
+  font: normal normal bold 17px/normal "Trebuchet MS", Helvetica, sans-serif;
+  color: rgba(255,255,255,0.9);
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  background: #ec176e;
+  -webkit-box-shadow: 2px 2px 2px 0 rgba(0,0,0,0.2) ;
+  box-shadow: 2px 2px 2px 0 rgba(0,0,0,0.2) ;
+  text-shadow: -1px -1px 0 rgba(15,73,168,0.66) ;
+  -webkit-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  -moz-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  -o-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+}
+
+.btn:hover {
+  color: rgba(198,188,188,0.9);
+  background: #a52e5f;
+}
+.btn2 {
+    margin:20px;
+  display: inline-block;
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  cursor: pointer;
+  padding: 11px 50px;
+  border: 1px solid #ff0085;
+  font: normal normal bold 17px/normal "Trebuchet MS", Helvetica, sans-serif;
+  color: rgba(255,255,255,0.9);
+  -o-text-overflow: clip;
+  text-overflow: clip;
+  background: #ec176e;
+  -webkit-box-shadow: 2px 2px 2px 0 rgba(0,0,0,0.2) ;
+  box-shadow: 2px 2px 2px 0 rgba(0,0,0,0.2) ;
+  text-shadow: -1px -1px 0 rgba(15,73,168,0.66) ;
+  -webkit-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  -moz-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  -o-transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+  transition: all 300ms cubic-bezier(0.42, 0, 0.58, 1);
+}
+
+.btn2:hover {
+  color: rgba(198,188,188,0.9);
+  background: #a52e5f;
+}
+.ansbox {
+  -webkit-box-sizing: content-box;
+  -moz-box-sizing: content-box;
+  box-sizing: content-box;
+  padding: 20px;
+  overflow: hidden;
+  border: none;
+  font: normal 16px/1 "Times New Roman", Times, serif;
+  color: rgba(255,255,255,1);
+  text-align: center;
+  -o-text-overflow: ellipsis;
+  text-overflow: ellipsis;
+  text-shadow: 1px 1px 1px rgba(0,0,0,0.2) ;
+}
+.shar-div{
+	width : 100%;
+	text-align:center;
+}
+  .share-fb {
+  text-align:center;
+  text-decoration: none;
+  color: #eee;
+  display: inline-block;
+  background-color: #3B5998;
+  font-weight: bold;
+    padding: 0.85rem 2rem;
+  width:25%;
+  border-radius: 3px;
+  font-size: 18px;
+}
+
+.share-fb:hover {
+  color: #eee;
+  text-decoration: none;
+  background-color: #355088; /*darken(#25d366, 5%);*/
+}.share-wa {
+    text-align: center;
+    float: left;
+    text-decoration: none;
+    color: #eee;
+    display: inline-block;
+    background-color: #20c47a;
+    font-weight: bold;
+    padding: 0.85rem 1rem;
+    width: 30%;
+    border-radius: 3px;
+    font-size: 20px;
+}
+.share-buttons-div {
+    width: 100%;
+	margin-left: 23%;
+    text-align: center;
+}
+@font-face {
+  font-family: 'icomoon';
+  src:  url('/fonts/icomoon.eot?fe2n1t');
+  src:  url('/fonts/icomoon.eot?fe2n1t#iefix') format('embedded-opentype'),
+    url('/fonts/icomoon.ttf?fe2n1t') format('truetype'),
+    url('/fonts/icomoon.woff?fe2n1t') format('woff'),
+    url('/fonts/icomoon.svg?fe2n1t#icomoon') format('svg');
+  font-weight: normal;
+  font-style: normal;
+}
+
+[class^="icon-"], [class*=" icon-"] {
+  /* use !important to prevent issues with browser extensions that change fonts */
+  font-family: 'icomoon' !important;
+  speak: none;
+  font-style: normal;
+  font-weight: normal;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+
+  /* Better Font Rendering =========== */
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.icon-facebook:before {
+  content: "\ea90";
+}
+.icon-whatsapp:before {
+  content: "\ea93";
+}
+table.greyGridTable {
+  border: 1px solid #FFFFFF;
+  width: 100%;
+  text-align: center;
+  border-collapse: collapse;
+}
+table.greyGridTable td, table.greyGridTable th {
+  border: 1px solid #FFFFFF;
+  padding: 3px 3px;
+}
+table.greyGridTable tbody td {
+  font-size: 13px;
+}
+table.greyGridTable td:nth-child(even) {
+  background: #EBEBEB;
+}
+table.greyGridTable thead {
+  background: #FFFFFF;
+  border-bottom: 4px solid #333333;
+}
+table.greyGridTable thead th {
+  font-size: 17px;
+  font-weight: bold;
+  color: #333333;
+  text-align: center;
+  border-left: 3px solid #333333;
+}
+table.greyGridTable thead th:first-child {
+  border-left: none;
+}
+
+table.greyGridTable tfoot td {
+  font-size: 14px;
+}</style>
+    </head><body>
+    <div class="top">My Favourites?</div>
+<div class="ques">
+    <h1>Ravi Sama Now Select Your Favourites and then Share it!</h1>
+    <form method="post" action="/save">
+        <input type="hidden" value="Ravi Sama" name="name"/>
+        <input type="submit" class="btn2" value="Select Here!"/>
+    </form>
+    </div>
+    <center>
+ 
+</body>
+</html>
